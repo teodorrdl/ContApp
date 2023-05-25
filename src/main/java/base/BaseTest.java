f7464487a8e52b60fc2f26f76ff59fa6d3c1bafa
DanaCatavei <dana_catavei@yahoo.com>
2023-05-25 22:00:09 +0300

    update

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,4 +1,73 @@
 package base;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.BasePage;
+
 public class BaseTest {
+
+    protected WebDriver driver;
+    Actions action;
+
+    public BaseTest(){
+        driver = BasePage.driverLocal;
+        PageFactory.initElements(driver,this);
+        action = new Actions(driver);
+    }
+
+    private WebDriverWait waitElement(){
+
+        return new WebDriverWait(driver, 15);
+    }
+
+    protected WebElement find(WebElement locator){
+        waitElement().until(ExpectedConditions.visibilityOf(locator) );
+        return locator;
+    }
+
+    protected void click(WebElement locator){
+
+        clickAble(locator).click();
+    }
+
+    protected WebElement clickAble(WebElement locator){
+        waitElement().until(ExpectedConditions.elementToBeClickable(locator));
+        return(locator);
+    }
+
+    private String getOperationSystem(){
+        String operatSystem = System.getProperty("os.name");
+        return operatSystem;
+    }
+
+    protected void clear(WebElement locator){
+        click(locator);
+        if(getOperationSystem().contains("Windows")) {
+            locator.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        }
+        else if(getOperationSystem().contains("Mac OS X")){
+            locator.sendKeys(Keys.chord(Keys.COMMAND, "a"));
+        }
+    }
+
+    protected void addText(WebElement locator, String inputText){
+        clear(locator);
+        locator.sendKeys(inputText);
+    }
+
+    protected String getPageTitle(){
+
+        return driver.getTitle();
+    }
+
+    public String getUrl(){
+
+        return driver.getCurrentUrl();
+    }
+
 }
--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,4 +1,39 @@
 package pages;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BasePage {
+
+    public static WebDriver driverLocal;
+    //pagina web care urmeaza sa o testam
+    String url = "https://semicolon-task-manager-skillbrains-grxarv6yb-cezarmocanu.vercel.app/login";
+    public static ExtentReports reports;
+    public static ExtentTest logger;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(ITestContext context){
+        //System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driverLocal = new ChromeDriver(options);
+        driverLocal.manage().window().maximize();
+        driverLocal.get(url);
+        context.setAttribute("WebDriver", driverLocal);
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+
+        driverLocal.quit();
+   }
+
+
 }
