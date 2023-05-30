62e28a2b6eabde5f76ea46ae9b268919f90d80f0
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-05-30 06:45:21 +0200

    update BaseTest

---
+++ src/test/java/tests/LoginPageContactAppTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.LoginPageContactApp;
+
+public class LoginPageContactAppTest extends BasePage {
+    public LoginPageContactApp loginPageContactApp() {
+        return new LoginPageContactApp();
+    }
+
+    @Test(description = "Happy Test!")
+    @Parameters({"username", "password"})
+    public void verifyLogin(String username, String password) {
+        loginPageContactApp().clickAuthenticationButton();
+        loginPageContactApp().addEmail(username);
+        loginPageContactApp().addPassword(password);
+        loginPageContactApp().clickRememberMeCheckbox();
+        loginPageContactApp().clickLoginButton();
+    }
+
+    @Test(description = "Verify the login page for both, when the field is blank and Submit button is clicked.")
+    @Parameters({"username", "password"})
+    public void verifyLoginWithBlankFields(String username, String password) {
+        loginPageContactApp().clickAuthenticationButton();
+        loginPageContactApp().addEmail(username);
+        loginPageContactApp().addPassword(password);
+        loginPageContactApp().clickRememberMeCheckbox();
+        loginPageContactApp().clickLoginButton();
+        loginPageContactApp().verifyAlertMessages();
+    }
+
+    @Test(description = "Verify if a user cannot login with a valid username and an invalid password.")
+    @Parameters({"username", "invalidPassword"})
+    public void verifyLoginWithInvalidPassword(String username, String invalidPassword) {
+        loginPageContactApp().clickAuthenticationButton();
+        loginPageContactApp().addEmail(username);
+        loginPageContactApp().addPassword(invalidPassword);
+        loginPageContactApp().clickRememberMeCheckbox();
+        loginPageContactApp().clickLoginButton();
+        loginPageContactApp().verifyAlertMessages();
+    }
+}
+
