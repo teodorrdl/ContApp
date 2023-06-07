89f9130a9eee2a1bed6780ba55ff30892c1124d6
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-06-07 22:30:30 +0200

    updated SettingCompanyTest.java

--- src/test/java/tests/SettingCompanyTest.java
+++ src/test/java/tests/SettingCompanyTest.java
@@ -1,8 +1,32 @@
 package tests;
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 import pages.BasePage;
+import pages.LoginPageContactApp;
+import pages.SettingCompany;
 
 public class SettingCompanyTest extends BasePage {
 
+    public SettingCompany settingCompany() {
+        return new SettingCompany();
+    }
 
+    public LoginPageContactApp loginPageContactApp() {
+        return new LoginPageContactApp();
+    }
+
+    @Test(description = "check the company settings if go to the correct url with valid login credentials " +
+            "and with the company already created")
+    @Parameters({"username", "password"})
+    public void verifySettingCompany(String username, String password) {
+        loginPageContactApp().clickAuthenticationButton();
+        loginPageContactApp().addEmail(username);
+        loginPageContactApp().addPassword(password);
+        loginPageContactApp().clickRememberMeCheckbox();
+        loginPageContactApp().clickLoginButton();
+        settingCompany().clickContAppPs();
+        settingCompany().clickDropdownButtonSetting();
+        settingCompany().clickSettingCompany();
+    }
 }
