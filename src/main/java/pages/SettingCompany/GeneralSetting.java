e7a4c9de58dfd69ad487e062ca8443340f31134b
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-06-12 19:27:31 +0200

    modified GeneralSetting.java

--- src/main/java/pages/SettingCompany/GeneralSetting.java
+++ src/main/java/pages/SettingCompany/GeneralSetting.java
@@ -1,10 +1,11 @@
 package pages.SettingCompany;
 
+import base.BaseTest;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
-public class General {
+public class GeneralSetting extends BaseTest {
     @FindBy(xpath = "//input[@placeholder='Caută după CUI/CIF']")
     private WebElement searchInput;
 
@@ -15,30 +16,39 @@ public class General {
     private WebElement addCompanyButton;
 
     @FindBy(xpath = "//input[@id='nr_reg_com']")
-    @CacheLookup
-    private WebElement nrRegComInput;
+    @CacheLookup private WebElement nrRegComInput;
 
     @FindBy(xpath = "(//span[@role='textbox'])[2]")
     @CacheLookup private WebElement entityTypeOption;
 
     @FindBy(xpath = "//span[@title='---']")
-    private WebElement entityTypeValue;
+    private WebElement entityOptionDefault;
 
     @FindBy(xpath = "//span[contains(@title,'Alta')]")
-    private WebElement entityTypeValueOther;
+    private WebElement entityTypeOptionOther;
 
     @FindBy(xpath = "//span[contains(@title,'Asociere de P.F.')]")
-    private WebElement entityTypeValueAssociate;
+    private WebElement entityTypeOptionAPF;
 
     @FindBy(xpath = "//span[normalize-space()='II']")
-    private WebElement entityTypeValueII;
+    private WebElement entityTypeOptionII;
 
     @FindBy(xpath = "//span[contains(text(),'Persoană fizică')]")
-    private WebElement entityTypeValuePF;
+    private WebElement entityTypeOptionPF;
 
     @FindBy(xpath = "//span[contains(text(),'PFA')]")
-    private WebElement entityTypeValuePFA;
+    private WebElement entityTypeOptionPFA;
 
     @FindBy(xpath = "//span[normalize-space()='PFI']")
-    private WebElement entityTypeValuePFI;
+    private WebElement entityTypeOptionPFI;
+
+    @FindBy (xpath = "//span[contains(text(),'Profesie Liberală')]")
+    private WebElement  entityTypeOptionPL;
+
+    @FindBy(xpath = "//span[normalize-space()='SA']")
+    private WebElement entityTypeOptionSA;
+
+    @FindBy(xpath = "//span[normalize-space()='SRL']")
+    private WebElement entityTypeOptionSRL;
+
 }
