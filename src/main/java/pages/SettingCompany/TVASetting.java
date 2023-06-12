4b9de76f53ba86394d81f23a93a43991d8b7c1c5
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-06-12 22:34:10 +0200

    renamed from SettingTVA.java

--- src/main/java/pages/SettingCompany/TVASetting.java
+++ src/main/java/pages/SettingCompany/TVASetting.java
@@ -4,7 +4,7 @@ import base.BaseTest;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class SettingTVA extends BaseTest {
+public class TVASetting extends BaseTest {
     @FindBy(xpath = "//a[normalize-space()='Setari TVA']")
     private WebElement settingTVALink;
 
