04df190e46085eab828986cd11cc9db4d43e5e61
Radulescu Teodor <teodor.rdl@gmail.com>
2023-06-13 14:14:30 +0300

    Added ContAppPaymentsPage/Test, Updated SmokeTestSuite.xml

--- src/main/java/pages/ContAppPaymentsPage.java
+++ src/main/java/pages/ContAppPaymentsPage.java
@@ -29,7 +29,7 @@ public class ContAppPaymentsPage extends BaseTest {
     private WebElement newSupplier;
     @FindBy(xpath = "//*[@id=\"registration-number\"]")
     private WebElement supplierCUI;
-    @FindBy(xpath ="//span[normalize-space()='România']")
+    @FindBy(xpath = "//span[normalize-space()='România']")
     private WebElement selectRomania;
     @FindBy(xpath = "//span[@id='select2-select-create-customer-org-type-select-container']")
     private WebElement supplierTypeSRL;
@@ -45,7 +45,7 @@ public class ContAppPaymentsPage extends BaseTest {
     private WebElement supplierBankName;
     @FindBy(xpath = "(//b[@role='presentation'])[11]")
     private WebElement supplierCountry;
-    @FindBy(xpath="//span[normalize-space()='Arges']")
+    @FindBy(xpath = "//span[normalize-space()='Arges']")
     private WebElement supplierCountySelect;
     @FindBy(xpath = "(//b[@role='presentation'])[12]")
     private WebElement supplierCounty;
@@ -71,20 +71,32 @@ public class ContAppPaymentsPage extends BaseTest {
     private WebElement openCreatedSupplier;
     @FindBy(xpath = "//span[normalize-space()='Q TEST SA']")
     private WebElement selectCreatedSupplier;
-    @FindBy(xpath = "//*[@id=\"select2-select-transaction-bank-id-expense---result-4the-8301\"]")
+    @FindBy(xpath = "(//b[contains(@role,'presentation')])[3]")
+    private WebElement openSelectBank;
+    @FindBy(xpath = "(//span[contains(text(),'Bancă principală')])[2]")
     private WebElement selectBank;
-    @FindBy(xpath = "//*[@id=\"select2-select-transaction-location-id-expense---result-c0ey-6616\"]")
+    @FindBy(xpath = "(//b[contains(@role,'presentation')])[4]")
+    private WebElement openSelectLocation;
+    @FindBy(xpath = "(//span[contains(text(),'Sediul profesional')])[2]")
     private WebElement selectLocation;
-    @FindBy(xpath = "//*[@id=\"select2-select-transaction-activity-id-expense---result-439b-Activitate nouă\"]")
+    @FindBy(xpath = "//span[contains(text(),'Activitate nouă')]")
     private WebElement addCaen;
-    @FindBy(xpath = "//*[@id=\"select2-select-activity-form-select-location--result-7nrt-6616\"]")
+    @FindBy(xpath = "(//span[contains(@title,'Sediul profesional')][normalize-space()='Sediul profesional'])[2]")
+    private WebElement openCaenLocation;
+    @FindBy(xpath = "//li[normalize-space()='Activitate desfășurată la terți']")
     private WebElement caenLocation;
-    @FindBy(xpath = "//*[@id=\"select2-select-activity-form-select-category--result-p486-1\"]")
+    @FindBy(xpath = "//*[@id=\"select2-select-activity-form-select-category--container\"]")
+    private WebElement openCaenCategory;
+    @FindBy(xpath = "//span[contains(text(),'Cedarea folosinței bunurilor calificată ca venitur')]")
     private WebElement caenCategory;
-    @FindBy(xpath = "//*[@id=\"select2-select-activity-form-select-activity--result-jj8g-5\"]")
+    @FindBy(xpath = "//input[@id='activity-form-input-documentNumber-']")
     private WebElement caenActivity;
     @FindBy(xpath = "//*[@id=\"activity-form-input-authorizationNumber-\"]")
     private WebElement caenNumberTextBox;
+    @FindBy(xpath = "//input[@id='activity-from-start-date-']")
+    private WebElement openCaenRegistrationDate;
+    @FindBy(xpath = "//input[@id='activity-from-end-date-']")
+    private WebElement openCaenSuspendDate;
     @FindBy(xpath = "/html/body/div[10]/div[1]/table/tbody/tr[2]/td[6]")
     private WebElement caenRegistrationDate;
     @FindBy(xpath = "/html/body/div[10]/div[1]/table/tbody/tr[4]/td[6]")
@@ -93,41 +105,80 @@ public class ContAppPaymentsPage extends BaseTest {
     private WebElement caenSaveButton;
     @FindBy(xpath = "//*[@id=\"select2-select-transaction-activity-id-expense---result-h2ze-2203\"]")
     private WebElement selectCreatedCaen;
-    @FindBy(xpath = "//*[@id=\"select2-select-receipt-document-selector-id-expense---result-w87f-1\"]")
+    @FindBy(xpath = "//span[@title='Extras Bancar']")
     private WebElement paymentDocument;
+    @FindBy(xpath = "//li[normalize-space()='Ordin de plata']")
+    private WebElement selectPaymentDocument;
     @FindBy(xpath = "//*[@id=\"input-number\"]")
     private WebElement documentNumber;
-    @FindBy(xpath = "/html/body/div[9]/div[1]/table/tbody/tr[2]/td[4]")
+    @FindBy(xpath = "//input[@id='transaction-date-expense--']")
     private WebElement documentDate;
-    @FindBy(xpath = "//*[@id=\"input-total\"]")
+    @FindBy(xpath = "//td[normalize-space()='10']")
+    private WebElement selectDocumentDate;
+    @FindBy(xpath = "//input[@type='number']")
     private WebElement leiPayment;
-    @FindBy(xpath = "//*[@id=\"select2-select-company-article-types-list-id-expense---result-e844-1\"]")
+    @FindBy(xpath = "//span[@title='Bunuri']")
     private WebElement itemType;
-    @FindBy(xpath = "//*[@id=\"select2-select-company-categories-list-id-expense---result-r678-8\"]")
+    @FindBy(xpath = "//li[normalize-space()='Altele']")
+    private WebElement selectItemType;
+    @FindBy(xpath = "//span[@id='select2-select-company-categories-list-id-expense---container']")
     private WebElement fiscalCategory;
-    @FindBy(xpath = "//*[@id=\"checkbox-saveArticle\"]")
+    @FindBy(xpath = "//li[normalize-space()='Cheltuieli integral nedeductibile']")
+    private WebElement fiscalCategoryOption;
+    @FindBy(xpath = "//input[@id='checkbox-saveArticle']")
     private WebElement saveItemCheckBox;
-    @FindBy(xpath = "/html/body/main/div/div/div[2]/div/div[2]/div/div/div/button")
+    @FindBy(xpath = "//button[contains(text(),'Adaugă plată')]")
     private WebElement addPaymentButton;
     @FindBy(xpath = "//span[@id='select2-select-company-articles-list-id-expense---container']")
     private WebElement explicationDropDown;
     @FindBy(xpath = "//input[@aria-label='Search']")
     private WebElement explicationSearchTextBox;
-
     @FindBy(xpath = "//span[normalize-space()='PFA']")
     private WebElement selectPFAButton;
 
     @FindBy(xpath = "//*[@id=\"onetrust-accept-btn-handler\"]")
     private WebElement closeCookieButton;
 
-    @FindBy(xpath = "//button[contains(@onclick,\"window.closeModal('customer-duplicate-modal-confirmation')\")][normalize-space()='Închide']")
+    @FindBy(xpath = "(//b[contains(@role,'presentation')])[5]")
+    private WebElement openCaenActivity;
+
+    @FindBy(xpath = "//*[@id=\"customer-duplicate-modal-confirmation\"]/div/div/div[3]/button[1]")
     private WebElement closeAnotherCompanyWarning;
+
+    public void clickSelectedDocumentDate() {
+        click(selectDocumentDate);
+    }
+
+    public void selectPaymentDocument() {
+        click(selectPaymentDocument);
+    }
+
+    public void clickItemType() {
+        click(selectItemType);
+    }
+
+    public void fiscalCategoryOption() {
+        click(fiscalCategoryOption);
+    }
+
     public void clickAutentification() {
         click(authenticationButton);
     }
 
     public void addEmail(String email) {
         addText(email, emailField);
+    }
+
+    public void openCaenRegistrationDate() {
+        click(openCaenRegistrationDate);
+    }
+
+    public void openCaenSuspendDate() {
+        click(openCaenSuspendDate);
+    }
+
+    public void openCaenCategory() {
+        click(openCaenCategory);
     }
 
     public void addPassword(String password) {
@@ -150,7 +201,9 @@ public class ContAppPaymentsPage extends BaseTest {
         click(payments);
     }
 
-    public void clickSupplier() {click(supplier);}
+    public void clickSupplier() {
+        click(supplier);
+    }
 
     public void clickAddNewPayment() {
         click(addNewPayment);
@@ -161,7 +214,7 @@ public class ContAppPaymentsPage extends BaseTest {
     }
 
     public void selectSupplier(String NewSupplier) {
-       addText(NewSupplier, newSupplier);
+        addText(NewSupplier, newSupplier);
     }
 
     public void addSupplierCUI(String CUI) {
@@ -176,87 +229,186 @@ public class ContAppPaymentsPage extends BaseTest {
         addText(SupplierName, supplierName);
     }
 
-    public void selectSupplierCountry(){click(supplierCountry);}
+    public void selectSupplierCountry() {
+        click(supplierCountry);
+    }
 
-    public void selectSupplierCounty(){click(supplierCounty);}
+    public void selectSupplierCounty() {
+        click(supplierCounty);
+    }
 
-    public void addSupplierCity(String City){addText(City,supplierCity);}
+    public void addSupplierCity(String City) {
+        addText(City, supplierCity);
+    }
 
-    public void addSupplierAddress(String Address){addText(Address, supplierAddress);}
+    public void addSupplierAddress(String Address) {
+        addText(Address, supplierAddress);
+    }
 
-    public void selectSupplierCurrency(){click(supplierCurrency);}
+    public void selectSupplierCurrency() {
+        click(supplierCurrency);
+    }
 
-    public void addSupplierIban(String Iban){addText(Iban, supplierIban);}
+    public void addSupplierIban(String Iban) {
+        addText(Iban, supplierIban);
+    }
 
-    public void addSupplierBankName(String BankName){addText(BankName, supplierBankName);}
+    public void addSupplierBankName(String BankName) {
+        addText(BankName, supplierBankName);
+    }
 
-    public void addSupplierContact(String Contact){addText(Contact, supplierContact);}
+    public void addSupplierContact(String Contact) {
+        addText(Contact, supplierContact);
+    }
 
-    public void addSupplierEmail(String Email){addText(Email, supplierEmail);}
+    public void addSupplierEmail(String Email) {
+        addText(Email, supplierEmail);
+    }
 
-    public void addSupplierPhone(String Phone){addText(Phone, supplierPhone);}
+    public void addSupplierPhone(String Phone) {
+        addText(Phone, supplierPhone);
+    }
 
-    public void addSupplierWebsite(String Website){addText(Website ,supplierWebsitte);}
+    public void addSupplierWebsite(String Website) {
+        addText(Website, supplierWebsitte);
+    }
 
-    public void saveSupplier(){click(supplierSaveButton);}
+    public void saveSupplier() {
+        click(supplierSaveButton);
+    }
 
-    public void openCreatedSupplier(){click(selectCreatedSupplier);}
+    public void openCreatedSupplier() {
+        click(openCreatedSupplier);
+    }
 
-    public void selectBank(){click(selectBank);}
+    public void selectBank() {
+        click(selectBank);
+    }
 
-    public void selectLocation(){click(selectLocation);}
+    public void selectLocation() {
+        click(selectLocation);
+    }
 
-    public void addNewCaen(){click(addCaen);}
+    public void addNewCaen() {
+        click(addCaen);
+    }
 
-    public void selectCaenLocation(){click(caenLocation);}
+    public void selectCaenLocation() {
+        click(caenLocation);
+    }
 
-    public void selectCaenCategory(){click(caenCategory);}
+    public void selectCaenCategory() {
+        click(caenCategory);
+    }
 
-    public void addCaenActivity(String CaenActivity){addText(CaenActivity,caenActivity);}
+    public void addCaenActivity(String CaenActivity) {
+        addText(CaenActivity, caenActivity);
+    }
 
-    public void addCaenNumber(String CaenNumber){addText(CaenNumber,caenNumberTextBox);}
+    public void addCaenNumber(String CaenNumber) {
+        addText(CaenNumber, caenNumberTextBox);
+    }
 
-    public void selectCaenRegistrationDate(){click(caenRegistrationDate);}
+    public void selectCaenRegistrationDate() {
+        click(caenRegistrationDate);
+    }
 
-    public void selectCaenSuspendDate(){click(caenSuspendDate);}
+    public void selectCaenSuspendDate() {
+        click(caenSuspendDate);
+    }
 
-    public void clickCaenSaveButton(){click(caenSaveButton);}
+    public void clickCaenSaveButton() {
+        click(caenSaveButton);
+    }
 
-    public void selectPaymentDocument(){click(paymentDocument);}
+    public void clickPaymentDocument() {
+        click(paymentDocument);
+    }
 
-    public void addDocumentNumber(String Document){addText(Document,documentNumber );}
+    public void addDocumentNumber(String Document) {
+        addText(Document, documentNumber);
+    }
 
-    public void selectDocumentDate(){click(documentDate);}
+    public void selectDocumentDate() {
+        click(documentDate);
+    }
 
-    public void addLeiPayment(String Lei){addText(Lei, leiPayment);}
+    public void addLeiPayment(String Lei) {
+        addText(Lei, leiPayment);
+    }
 
-    public void selectItemType(){click(itemType);}
+    public void selectItemType() {
+        click(itemType);
+    }
 
-    public void clickExplicationDropDown(){click(explicationDropDown);}
+    public void clickExplicationDropDown() {
+        click(explicationDropDown);
+    }
 
-    public void addExplication(String Explication){addText(Explication,explicationSearchTextBox);}
+    public void addExplication(String Explication) {
+        addText(Explication, explicationSearchTextBox);
+    }
 
-    public void selectFiscalCategory(){click(fiscalCategory);}
+    public void selectFiscalCategory() {
+        click(fiscalCategory);
+    }
 
-    public void checkSaveItem(){click(saveItemCheckBox);}
+    public void checkSaveItem() {
+        click(saveItemCheckBox);
+    }
 
-    public void addPayment(){click(addPaymentButton);}
+    public void addPayment() {
+        movetoElement();
+        click(addPaymentButton);
+    }
 
-    public void clickSRL(){click(supplierTypeSRL);}
+    public void clickSRL() {
+        click(supplierTypeSRL);
+    }
 
-    public void clickPFA(){click(selectPFAButton);}
+    public void clickPFA() {
+        click(selectPFAButton);
+    }
 
-    public void selectRomania(){click(selectRomania);}
+    public void selectRomania() {
+        click(selectRomania);
+    }
 
-    public void closeCookieButton(){click(closeCookieButton);}
+    public void closeCookieButton() {
+        click(closeCookieButton);
+    }
 
-    public void selectCounty(){click(supplierCountySelect);}
+    public void selectCounty() {
+        click(supplierCountySelect);
+    }
 
-    public void changeCurrency(){click(changeCurrency);}
+    public void changeCurrency() {
+        click(changeCurrency);
+    }
 
-    public void selectCreatedSupplier(){click(selectCreatedSupplier);}
+    public void selectCreatedSupplier() {
+        click(selectCreatedSupplier);
+    }
 
-    public void closeAnotherCompanyWarning(){click(closeAnotherCompanyWarning);}
+    public void closeAnotherCompanyWarning() {
+        click(closeAnotherCompanyWarning);
+    }
+
+    public void openSelectBank() {
+        click(openSelectBank);
+    }
+
+    public void openSelectLocation() {
+        click(openSelectLocation);
+    }
+
+    public void openCaenActivity() {
+        click(openCaenActivity);
+    }
+
+    public void openCaenLocation() {
+        click(openCaenLocation);
+    }
 
 
 }
--- src/test/java/tests/ContAppPaymentTest.java
+++ src/test/java/tests/ContAppPaymentTest.java
@@ -7,16 +7,17 @@ import pages.ContAppPaymentsPage;
 
 public class ContAppPaymentTest extends BasePage {
 
-    public  ContAppPaymentsPage paymentPage() {
+    public ContAppPaymentsPage paymentPage() {
         return new ContAppPaymentsPage();
     }
+
     @Test(description = "Add New Payment")
-    @Parameters({"email", "password","CUI", "RegistrationNumber","SupplierName","City","Address","IBAN",
-            "BankName","Contact", "SupplierEmail", "SupplierWebsite", "SupplierPhone", "CaenNumber",
+    @Parameters({"email", "password", "CUI", "RegistrationNumber", "SupplierName", "City", "Address", "IBAN",
+            "BankName", "Contact", "SupplierEmail", "SupplierWebsite", "SupplierPhone", "CaenNumber",
             "CaenActivity", "DocumentNumber", "Lei", "Explication", "NewSupplier"})
     public void addNewPayment(String email, String password, String CUI, String RegistrationNumber, String SupplierName, String City,
                               String Address, String IBAN, String BankName, String Contact, String SupplierEmail, String SupplierPhone,
-                              String SupplierWebsite,String NewSupplier ,String CaenNumber, String CaenActivity, String DocumentNumber,
+                              String SupplierWebsite, String NewSupplier, String CaenNumber, String CaenActivity, String DocumentNumber,
                               String Lei, String Explication) {
         paymentPage().clickAutentification();
         paymentPage().addEmail(email);
@@ -28,7 +29,7 @@ public class ContAppPaymentTest extends BasePage {
         paymentPage().clickAddNewPayment();
         paymentPage().selectPaymentType();
         paymentPage().clickSupplier();
-        paymentPage().selectSupplier(NewSupplier+"\n");
+        paymentPage().selectSupplier(NewSupplier + "\n");
         paymentPage().addSupplierCUI(CUI);
         paymentPage().addSupplierRegistrationNumber(RegistrationNumber);
         paymentPage().clickSRL();
@@ -53,31 +54,37 @@ public class ContAppPaymentTest extends BasePage {
         paymentPage().closeAnotherCompanyWarning();
         paymentPage().openCreatedSupplier();
         paymentPage().selectCreatedSupplier();
+        paymentPage().openSelectBank();
         paymentPage().selectBank();
+        paymentPage().openSelectLocation();
         paymentPage().selectLocation();
+        paymentPage().openCaenActivity();
         paymentPage().addNewCaen();
+        paymentPage().openCaenLocation();
         paymentPage().selectCaenLocation();
+        paymentPage().openCaenCategory();
         paymentPage().selectCaenCategory();
         paymentPage().addCaenActivity(CaenActivity);
         paymentPage().addCaenNumber(CaenNumber);
+        paymentPage().openCaenRegistrationDate();
         paymentPage().selectCaenRegistrationDate();
+        paymentPage().openCaenSuspendDate();
         paymentPage().selectCaenSuspendDate();
         paymentPage().clickCaenSaveButton();
+        paymentPage().clickPaymentDocument();
         paymentPage().selectPaymentDocument();
         paymentPage().addDocumentNumber(DocumentNumber);
         paymentPage().selectDocumentDate();
+        paymentPage().clickSelectedDocumentDate();
         paymentPage().addLeiPayment(Lei);
         paymentPage().selectItemType();
+        paymentPage().clickItemType();
         paymentPage().clickExplicationDropDown();
         paymentPage().addExplication(Explication);
         paymentPage().selectFiscalCategory();
-        paymentPage().checkSaveItem();
+        paymentPage().fiscalCategoryOption();
         paymentPage().addPayment();
 
-
-
     }
-
-
 
 }
