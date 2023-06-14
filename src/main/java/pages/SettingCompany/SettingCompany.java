2c855fb0e7b2d4178288c445d627c3ac2382841b
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-06-14 18:47:37 +0200

    modified SettingCompany

--- src/main/java/pages/SettingCompany/SettingCompany.java
+++ src/main/java/pages/SettingCompany/SettingCompany.java
@@ -2,7 +2,6 @@ package pages.SettingCompany;
 
 import base.BaseTest;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
