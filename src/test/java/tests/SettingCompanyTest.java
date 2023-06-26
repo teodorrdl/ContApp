b71696943e5d0609f770959d2291724a92ba9359
DanielIrimia87 <87778687+DanielIrimia87@users.noreply.github.com>
2023-06-26 07:06:40 +0200

    modified SettingCompanyTest.java

--- src/test/java/tests/SettingCompanyTest.java
+++ src/test/java/tests/SettingCompanyTest.java
@@ -32,8 +32,16 @@ public class SettingCompanyTest extends BasePage {
         return new ActivityPage();
     }
 
+    public CurrencyPage currencyPage() {
+        return new CurrencyPage();
+    }
+
+    public HousesAndBanks housesAndBanks() {
+        return new HousesAndBanks();
+    }
+
     @Test(description = "check the company settings if direct to the correct url with valid login credentials " +
-            "and with the company already created",priority = 2)
+            "and with the company already created", priority = 2)
     @Parameters({"username", "password"})
     public void verifySettingCompany(String username, String password) {
         loginPageContactApp().clickAuthenticationButton();
@@ -49,9 +57,9 @@ public class SettingCompanyTest extends BasePage {
     @Test(description = "modified the company at the value organization type with 'persoana fizica' from 'Alta' value ", priority = 1)
     @Parameters({"username", "password", "entityType", "cnp", "firstName", "lastName", "country", "county", "city",
             "address", "email", "phone"})
-    public void modifyCompanyOrganisationType( String username, String password, String entityType, String cnp,
-                                               String firstName, String lastName, String country, String county,
-                                               String city, String address, String email, String phone) {
+    public void modifyCompanyOrganisationType(String username, String password, String entityType, String cnp,
+                                              String firstName, String lastName, String country, String county,
+                                              String city, String address, String email, String phone) {
         loginPageContactApp().clickAuthenticationButton();
         loginPageContactApp().addEmail(username);
         loginPageContactApp().addPassword(password);
@@ -79,7 +87,7 @@ public class SettingCompanyTest extends BasePage {
     }
 
     @Test(description = "Add period of activity with CIF Intra-Community Transactions", priority = 4)
-    @Parameters({"username", "password", "CIF", "registrationDate","cancellationDate"})
+    @Parameters({"username", "password", "CIF", "registrationDate", "cancellationDate"})
     public void addPeriodOfActivityCIF(String username, String password, String CIF, String registrationDate, String cancellationDate) {
         loginPageContactApp().clickAuthenticationButton();
         loginPageContactApp().addEmail(username);
@@ -125,7 +133,7 @@ public class SettingCompanyTest extends BasePage {
 
     @Test(description = "Add authorized activities", priority = 7)
     @Parameters({"username", "password", "activityName", "incomeCategory",
-            "sourceOfIncome","authorizationNr","registrationDate","endedDate"})
+            "sourceOfIncome", "authorizationNr", "registrationDate", "endedDate"})
     public void addAuthorizedActivities(String username, String password, String activityName, String incomeCategory,
                                         String sourceOfIncome, String authorizationNr, String registrationDate, String endedDate) {
         loginPageContactApp().clickAuthenticationButton();
@@ -154,5 +162,54 @@ public class SettingCompanyTest extends BasePage {
         activityPage().clickActivityLink();
         activityPage().deleteAuthorizedActivity();
     }
+
+    @Test(description = "Add new currency", priority = 9)
+    @Parameters({"username", "password", "currencyName", "currencySymbol"})
+    public void addNewCurrency(String username, String password, String currencyName, String currencySymbol) {
+        loginPageContactApp().clickAuthenticationButton();
+        loginPageContactApp().addEmail(username);
+        loginPageContactApp().addPassword(password);
+        loginPageContactApp().clickRememberMeCheckbox();
+        loginPageContactApp().clickLoginButton();
+        settingCompany().clickContAppPs();
+        settingCompany().clickDropdownButtonSetting();
+        settingCompany().clickSettingCompany();
+        currencyPage().clickCurrencyLink();
+        currencyPage().addNewCurrency(currencyName, currencySymbol);
+        currencyPage().checkCurrencyAdded(currencyName, currencySymbol);
+    }
+
+    @Test(description = "Add Bank", priority = 10)
+    @Parameters({"username", "password", "typeofService", "bankName", "currency", "bankIBAN", "bankDetails"})
+    public void addBank(String username, String password, String typeofService, String bankName, String currency, String bankIBAN, String bankDetails) {
+        loginPageContactApp().clickAuthenticationButton();
+        loginPageContactApp().addEmail(username);
+        loginPageContactApp().addPassword(password);
+        loginPageContactApp().clickRememberMeCheckbox();
+        loginPageContactApp().clickLoginButton();
+        settingCompany().clickContAppPs();
+        settingCompany().clickDropdownButtonSetting();
+        settingCompany().clickSettingCompany();
+        housesAndBanks().clickHousesAndBanksLink();
+        housesAndBanks().addNewBankOrHouse(typeofService, bankName, currency, bankIBAN, bankDetails);
+        housesAndBanks().checkBankOrHouseAdded(typeofService, bankName, currency, bankIBAN);
+    }
+
+    @Test(description = "Add House", priority = 11)
+    @Parameters({"username", "password", "typeofService", "name", "currency", "cashRegisterIBAN", "cashRegisterDetails"})
+    public void addCashRegister(String username, String password, String typeofService, String name, String currency, String cashRegisterIBAN, String cashRegisterDetails) {
+        loginPageContactApp().clickAuthenticationButton();
+        loginPageContactApp().addEmail(username);
+        loginPageContactApp().addPassword(password);
+        loginPageContactApp().clickRememberMeCheckbox();
+        loginPageContactApp().clickLoginButton();
+        settingCompany().clickContAppPs();
+        settingCompany().clickDropdownButtonSetting();
+        settingCompany().clickSettingCompany();
+        housesAndBanks().clickHousesAndBanksLink();
+        housesAndBanks().addNewBankOrHouse(typeofService, name, currency, cashRegisterIBAN, cashRegisterDetails);
+        housesAndBanks().checkBankOrHouseAdded(typeofService, name, currency, cashRegisterIBAN);
+    }
+
 
 }
