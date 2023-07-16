7a5067594c1f01697e8e6fcce7254137dbfc1706
Gabriela7-7 <118394027+Gabriela7-7@users.noreply.github.com>
2023-07-16 20:09:34 +0300

    Tema AdaugaVenituri

---
+++ src/test/java/tests/AdaugaVenituriTest.java
@@ -0,0 +1,52 @@
+package tests;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pages.AdaugaVenituri;
+import pages.BasePage;
+import pages.LoginPageContactApp;
+
+
+public class AdaugaVenituriTest extends BasePage {
+    public LoginPageContactApp loginPage() {
+        return new LoginPageContactApp();
+    }
+
+    public AdaugaVenituri page() {
+        return new AdaugaVenituri();
+    }
+
+
+    @Test(description = "adauga venituri")
+    @Parameters({"email", "password", "contAppPs", "configurare", "venituri",
+            "articoleVenituri ","adaugaArticolNou ",
+            "denumireArticol ", " valuta", " pretVanzare", " tipArticol", " categorieFiscala", " unitateDeMasura",
+            " salveaza"})
+
+    public void AdaugaVenituri(String textemail, String textPassword, String ContAppPs, String configurare, String venituri, String articoleVenituri,String adaugaArticolNou,
+                               String denumireArticol, String valuta, String pretVanzare, String tipArticol, String categorieFiscala,
+                               String unitateDeMasura, String salveaza) {
+
+
+        loginPage().clickAuthenticationButton();
+        loginPage().addEmail(textemail);
+        loginPage().addPassword(textPassword);
+        loginPage().clickRememberMeCheckbox();
+        loginPage().clickLoginButton();
+
+        page().clickContAppPs();
+        page().clickVenituri();
+        page().clickArticoleVenituri();
+        page().clickAdaugaArticolNou();
+        page().addDenumireArticol();
+        page().clickValuta();
+        page().clickPretVanzare();
+        page().clickTipArticol();
+        page().clickCategorieFiscala();
+        page().clickUnitateDeMasura();
+        page().clickSalveaza();
+
+
+    }
+
+}
